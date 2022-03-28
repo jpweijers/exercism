22843c63588c79f6190332c35fda919c7679feaa
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-03-28 11:06:38 +0200

    grains improments

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,14 +1,8 @@
-from multiprocessing.sharedctypes import Value
-
-
 def square(number):
     if number not in range(1, 65):
         raise ValueError("square must be between 1 and 64")
-    return 1 * 2 ** (number - 1)
+    return 2 ** (number - 1)
 
 
 def total():
-    total = 0
-    for i in range(1, 65):
-        total += square(i)
-    return total
+    return 2 ** 64 - 1
