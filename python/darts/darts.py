4fde463f726053254979a4f4c60798c67b245d14
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-03-28 11:30:51 +0200

    darts done

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,13 @@
+def _coordinates_in_circle(x, y, radius):
+    return x**2 + y**2 <= radius**2
+
+
 def score(x, y):
-    pass
+    if _coordinates_in_circle(x, y, 1):
+        return 10
+    elif _coordinates_in_circle(x, y, 5):
+        return 5
+    elif _coordinates_in_circle(x, y, 10):
+        return 1
+    else:
+        return 0
