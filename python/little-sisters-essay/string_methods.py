5f84922cd8d2ac942a798a8ea3074f15883b7a90
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-03-29 08:56:14 +0200

    little sisters essay done

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,3 +1,6 @@
+from hashlib import new
+
+
 def capitalize_title(title):
     """
 
@@ -5,7 +8,7 @@ def capitalize_title(title):
     :return:  str title string in title case (first letters capitalized)
     """
 
-    pass
+    return title.title()
 
 
 def check_sentence_ending(sentence):
@@ -15,7 +18,7 @@ def check_sentence_ending(sentence):
     :return:  bool True if punctuated correctly with period, False otherwise.
     """
 
-    pass
+    return sentence.endswith('.')
 
 
 def clean_up_spacing(sentence):
@@ -25,7 +28,7 @@ def clean_up_spacing(sentence):
     :return: str a sentence that has been cleaned of leading and trailing space characters.
     """
 
-    pass
+    return sentence.strip()
 
 
 def replace_word_choice(sentence, old_word, new_word):
@@ -37,4 +40,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :return:  str input sentence with new words in place of old words
     """
 
-    pass
+    return sentence.replace(old_word, new_word)
