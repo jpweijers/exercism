13a918b70d75dc84086cf3bc6130882fb1e52f06
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-03-30 15:59:14 +0200

    matrix done

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,20 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = matrix_string
+        self.rows = self._get_rows()
+        self.col_count = self._count_cols()
+
+    def _get_rows(self):
+        rows = self.matrix.split('\n')
+
+        return [[int(n) for n in row.split()] for row in rows]
+
+    def _count_cols(self):
+        return len(self.matrix.split('\n'))
 
     def row(self, index):
-        pass
+        return self.rows[index-1]
 
     def column(self, index):
-        pass
+
+        return [self.row(i)[index-1] for i in range(1, self.col_count + 1)]
