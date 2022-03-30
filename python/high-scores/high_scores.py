2f266157e06d05ec396ac20a89b3c9d8941e79bb
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-03-30 14:56:30 +0200

    high-scores done

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,12 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    scores.sort()
+    return scores[-1]
 
 
 def personal_top_three(scores):
-    pass
+    scores.sort(reverse=True)
+    return scores[:3]
