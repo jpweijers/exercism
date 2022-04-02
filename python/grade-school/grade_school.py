66181b37bffaaaea71c06105df2f055975052b24
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-04-02 18:37:19 +0200

    grade-school done

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,15 +1,30 @@
 class School:
     def __init__(self):
-        pass
+        self.students = {}
+        self.add_list = []
 
     def add_student(self, name, grade):
-        pass
+        if name not in self.students:
+            self.add_list.append(True)
+            self.students[name] = grade
+        else:
+            self.add_list.append(False)
 
     def roster(self):
-        pass
+        grades = list(set(g for g in self.students.values()))
+        grades.sort()
+
+        roster = []
+        for g in grades:
+            roster += self.grade(g)
+
+        return roster
 
     def grade(self, grade_number):
-        pass
+        grade_students = [s for s, g in self.students.items()
+                          if g == grade_number]
+        grade_students.sort()
+        return grade_students
 
     def added(self):
-        pass
+        return self.add_list
