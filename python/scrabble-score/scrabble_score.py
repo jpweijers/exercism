2573360b7b866879403a74e7a2bbd156c4a8e914
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-04-02 16:50:39 +0200

    scrabble-score done

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,17 @@
+LETTER_VALUES = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"]
+}
+
+LETTER_VALUES = {l: score for score, letters in LETTER_VALUES.items()
+                 for l in letters}
+
+
 def score(word):
-    pass
+    print(LETTER_VALUES)
+    return sum(LETTER_VALUES[l.upper()] for l in word)
