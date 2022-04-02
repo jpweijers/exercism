1346842815f3548d8c09985afdda84ed6719a7bc
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-04-02 14:35:05 +0200

    hamming end

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,5 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    return len([a for a, b in zip(strand_a, strand_b) if a != b])
